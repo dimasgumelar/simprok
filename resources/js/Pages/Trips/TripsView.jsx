41b34128c04500c53978490949fbe8929d150707
dimasgumelar <dimasgumelar.9@gmail.com>
2026-01-22 09:13:44 +0700

    add caption

--- resources/js/Pages/Trips/TripsView.jsx
+++ resources/js/Pages/Trips/TripsView.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { Head } from "@inertiajs/react";
 import axios from "axios";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import TripMap from "@/Components/Maps/TripView";
+import { parseDateTimeFull } from "@/utils/helper-function";
 
 export default function TripsView({ identifier, tripId }) {
     const [trips, setTrips] = useState([]);
@@ -34,12 +35,26 @@ export default function TripsView({ identifier, tripId }) {
         return () => (stop = true);
     }, [page, tripId]);
 
+    const startDate = useMemo(
+        () => (trips.length > 0 ? trips[0].recorded_at : null),
+        [trips],
+    );
+
+    const endDate = useMemo(
+        () => (trips.length > 0 ? trips[trips.length - 1].recorded_at : null),
+        [trips],
+    );
+
     return (
         <AuthenticatedLayout>
             <Head title="Kecepatan" />
             <div className="card bg-base-100 shadow-sm w-full">
                 <div className="card-body">
                     <div className="overflow-x-auto">
+                        <h2 className="font-bold text-base mb-3">
+                            Trip : {tripId} ({parseDateTimeFull(startDate)} -{" "}
+                            {parseDateTimeFull(endDate)})
+                        </h2>
                         <TripMap trips={trips} />
                     </div>
                 </div>
