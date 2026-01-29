51b453d90d68d7f6df5e176ee314c3bfe2edeb00
dimasgumelar <dimasgumelar.9@gmail.com>
2026-01-29 07:57:41 +0700

    build from lokal

---
+++ public/build/assets/Constant-8i-6LVST.js
@@ -0,0 +1,1 @@
+import{j as r,Y as s}from"./app-ZpQNyCnu.js";function o(){return r.jsx(s,{href:route("gpslogs.index"),children:"GPS Log"})}export{o as BreadcrumbsGpsLogs};
---
+++ public/build/assets/Constant-CM8TQdMo.js
@@ -0,0 +1,1 @@
+import{j as e,Y as r}from"./app-ZpQNyCnu.js";function t(){return e.jsx(r,{href:route("devices.index"),children:"Perangkat"})}export{t as BreadcrumbsDevices};
